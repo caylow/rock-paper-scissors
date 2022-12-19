32d797abd26eae7886e41e61fa502b78107eb885
caaylow <jupbiondo@gmail.com>
2022-12-19 20:47:46 -0300

    Added Computer choices, now it's playable in console

--- main.js
+++ main.js
@@ -1,31 +1,34 @@
 function playRound(playerSelection, computerSelection){
 
     // Case sensitive
-    playerSelection.toLowerCase();
-    computerSelection.toLowerCase();
-
+    playerSelection = formalyzeStr(playerSelection);
+    computerSelection = formalyzeStr(computerSelection);
     //Evaluates the round results and tells if there's a winner or if it's a tie.
     if(isPlayerWinner(playerSelection, computerSelection)){
-        return "You Win! " + playerSelection + " beats" + computerSelection;
+        return "You Win! " + playerSelection + " beats " + computerSelection;
     }else if(isATie(playerSelection, computerSelection)){
         return "It's a tie!";
     }else{
-        return "You Lose! " + computerSelection + " beats" + playerSelection;
+        return "You Lose! " + computerSelection + " beats " + playerSelection;
     }
 }
 
+function formalyzeStr(astring){
+    return astring.charAt(0).toUpperCase() + astring.slice(1);
+}
 //Returns true if player did win
 function isPlayerWinner(playerSelection, computerSelection){
     const winnerConditions = [
-        playerSelection == "rock" && computerSelection == "scissors",
-        playerSelection == "scissors" && computerSelection == "paper",
-        playerSelection == "paper" && computerSelection == "rock",
+        playerSelection == "Rock" && computerSelection == "Scissors",
+        playerSelection == "Scissors" && computerSelection == "Paper",
+        playerSelection == "Paper" && computerSelection == "Rock",
     ]
     acum = 0;
     do{
         if(winnerConditions[acum]){
             return true;
         }
+        acum++;
     }while(acum < 3);
     return false;
 }
@@ -35,7 +38,20 @@ function isATie(playerSelection, computerSelection){
     return playerSelection == computerSelection;
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+//Returns values between 0 and less than max's value
+function getRandomInt(max){
+    return Math.floor(Math.random() * max);
+}
 
-console.log(playRound(playerSelection, computerSelection));
+//Returns a random choice of the computer.
+function getComputerChoice(){
+    choices = ["rock", "paper", "scissors"];
+    return choices[getRandomInt(3)];
+}
+
+let playerSelection = "rock";
+const computerSelection = getComputerChoice();
+for(let i = 0; i<5; i++){
+    playerSelection = prompt("Enter your selection: ");
+    console.log(playRound(playerSelection, computerSelection));
+}
