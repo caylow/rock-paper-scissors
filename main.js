4f026a219ad61593c0b388a5e0aae045ab54c057
caaylow <jupbiondo@gmail.com>
2022-12-20 16:07:28 -0300

    Added style, and js classes to make the game 1 round playable

--- main.js
+++ main.js
@@ -5,11 +5,12 @@ function playRound(playerSelection, computerSelection){
     computerSelection = formalyzeStr(computerSelection);
     //Evaluates the round results and tells if there's a winner or if it's a tie.
     if(isPlayerWinner(playerSelection, computerSelection)){
-        return "You Win! " + playerSelection + " beats " + computerSelection;
+        return 1;
     }else if(isATie(playerSelection, computerSelection)){
-        return "It's a tie!";
+        return 0;
     }else{
-        return "You Lose! " + computerSelection + " beats " + playerSelection;
+        return -1;
+        
     }
 }
 
@@ -49,9 +50,43 @@ function getComputerChoice(){
     return choices[getRandomInt(3)];
 }
 
-let playerSelection = "rock";
-const computerSelection = getComputerChoice();
-for(let i = 0; i<5; i++){
-    //playerSelection = prompt("Enter your selection: ");
-    console.log(playRound(playerSelection, computerSelection));
+function showSelection(playerSelection, gameChoice, isUser=false){
+    if(isUser){
+        gameChoice.classList.remove('fa-user');
+    }else{
+        gameChoice.classList.remove('fa-computer'); 
+        gameChoice.classList.remove('fa-solid'); 
+        gameChoice.classList.add('fa-regular'); 
+    }
+    if(playerSelection=='paper'){
+        gameChoice.classList.add('fa-hand');
+    }else{
+        gameChoice.classList.add('fa-hand-'+playerSelection);
+    }
 }
+
+function showResults(playRound, winMessage, loseMessage){
+    if(playRound==1){
+        winMessage.setAttribute('style', 'display: inline;');  
+    }else if(playRound==0){
+        return;
+    }else{
+        return loseMessage.setAttribute('style', 'display: inline;'); 
+    }
+}
+
+const playerBox = document.querySelector('#player-user');
+const computerBox = document.querySelector('#player-pc');
+const winText = document.querySelector('#win-text');
+const loseText = document.querySelector('#lose-text');
+const gameItems = document.querySelectorAll('.game-item');
+gameItems.forEach((gameItem) =>{
+    gameItem.addEventListener('click', ()=>{
+        let computerChoice = getComputerChoice();
+        showSelection(gameItem.id, playerBox, true);
+        showSelection(computerChoice, computerBox);
+        showResults(playRound(gameItem.id, computerChoice), winText,loseText);
+    })
+})
+
+
